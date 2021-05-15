10976ed19e7084985c0142dab5406642ef06d43d
Kirk Jones <kirk.r.jones@me.com>
2021-05-15 17:37:41 -0400

    Posts now show comments.
    Basic functionality completed. Will come back later and do UI and add authors and users.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  # before_action :set_post, only: [:show]
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.includes(:comments).find(params[:id])
   end
 
   def new
